5b2c9bf76b0b0d32af5231fcedef43a3b2bbae7c
KEEN <homurahomu@naver.com>
2021-10-06 14:29:55 +0900

    신조어 목록 추가

--- NeologismApp/ViewController.swift
+++ NeologismApp/ViewController.swift
@@ -19,8 +19,22 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var resultLabel: UILabel!
   @IBOutlet weak var textField: UITextField!
+  
   // MARK: Properties
-  let newWords: [String: String] = ["킹받네": "엄청 열 받네", "삼귀자": "사귀기 전 단계인 썸 단계"]
+  let newWords: [String: String] = [
+    "킹받네": "엄청 열 받네",
+    "삼귀자": "사귀기 전 단계인 썸 단계",
+    "구취": "구독 취소",
+    "스세권": "스타벅스 근처 거주지",
+    "갑통알": "갑자기 통장을 보니 알바해야 할 것 같다.",
+    "만반잘부": "만나서 반가워 잘 부탁해",
+    "꾸안꾸": "꾸민듯 안꾸민듯 꾸민 스타일",
+    "오저치고": "오늘 저녁 치킨 고?",
+    "별다줄": "벌걸 다 줄이네;;",
+    "오놀아놈": "오~ 놀 줄 아는 놈인가",
+    "자만추": "자연스러운 만남 추구",
+    "사바사": "사람 by 사람 (사람마다 다르다)"
+  ]
   
   // MARK: View Life-Cycle
   override func viewDidLoad() {
