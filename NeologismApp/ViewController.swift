cd7425b5851a6a97a6223c6a8fc7fddf2dcbcde8
KEEN <homurahomu@naver.com>
2021-10-06 14:25:04 +0900

    검색버튼 기능 구현

--- NeologismApp/ViewController.swift
+++ NeologismApp/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UIViewController {
   @IBOutlet weak var tagThreeButotn: UIButton!
   @IBOutlet weak var tagFourButotn: UIButton!
   
+  @IBOutlet weak var resultLabel: UILabel!
+  @IBOutlet weak var textField: UITextField!
+  // MARK: Properties
+  let newWords: [String: String] = ["킹받네": "엄청 열 받네", "삼귀자": "사귀기 전 단계인 썸 단계"]
+  
   // MARK: View Life-Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -40,6 +45,13 @@ class ViewController: UIViewController {
 
   // MARK: Action
   @IBAction func onSearch(_ sender: UIButton) {
+    let key = textField.text ?? ""
+    
+    if let exsistValue = newWords[key] {
+      resultLabel.text = exsistValue
+    } else {
+      resultLabel.text = "일치하는 신조어가 없습니다."
+    }
   }
 }
 
